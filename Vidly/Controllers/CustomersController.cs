2a85ed8b2916c1f057474d67368e3073f509fe16
Ahmed <aheid996@gmail.com>
2021-06-19 15:29:20 +0200

    InitialModel

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer(){Id = 1 ,Name = "Ahmed"},
+                new Customer(){Id = 2, Name = "Mohamed"}
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(Customer customer)
+        {
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Ahmed Eid" };
+            return View(movie);
+        }
+        public ActionResult Edit(int movieId)
+        {
+            return Content("movieId = " + movieId);
+        }
+
+        [Route("Movies/Released/{year:regex(\\d{4}):range(2000,2021)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult Released(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
